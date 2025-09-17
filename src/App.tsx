a29be9ce2dd836b816ce78c5480863a726adb8e9
programistress <programistress@gmail.com>
2025-09-17 10:24:31 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1>Meow or Woof Recognizer</h1>
+    <p>Upload an image of a cat or dog to see if it's a cat or a dog.</p>
+    <input type="file" accept="image/*" />
+    <button>Upload</button>
+    <p>Result: </p>
     </div>
   );
 }
